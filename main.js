38e7b5b7e41667da4ad09b7bb1a53a75e20d1f77
ManbirK <72926520+ManbirK@users.noreply.github.com>
2022-03-22 06:15:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log("Hello World");
+
+var first_name="Canada";
+console.log(first_name);
+console.log(first_name.length);
+
+var last_name = " Flag";
+console.log(last_name);
+console.log(last_name.length);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+console.log(full_name.length);
+
+
+var new_name = full_name.replace("Flag","Baby Yoda");
+console.log(new_name);
+
+
+console.log(full_name.replace("Flag","Pencil"));
+
+console.log(first_name.toUpperCase());
+
+console.log(last_name.toUpperCase());
+
+console.log(last_name.toLowerCase());
+
+var num_1 = 121;
+
+console.log(Math.sqrt(num_1));
+
+console.log(Math.sqrt(16478059402734983247298749283));
+
+// 45,35,78,3,4,2,12,4577
+
+console.log(Math.min(45,35,78,3,4,258,12,4577));
+
+
+
+console.log(Math.max(900966,233433,23476543,345763,12536476,124356412345765999999999999779897));
+
+// 4*4*4*4*4  4 raise to the power of 5
+console.log(Math.pow(4,5));
+console.log(Math.pow(098764467,8535798643679963));
+
+
+
+
+
+
+
+
